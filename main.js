1a67bfe4f4fce1a39c4f52eecb2062541ba43757
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-05-13 11:41:08 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const numberOfRounds = process.argv[2];
+const weapons = ['rock' , 'paper', 'scissors'];
+const weaponOfChoice = () => {
+  return weapons[parseInt(Math.random()*weapons.length) %3];
+}
+const result = (winLoseDraw) => {
+  switch (winLoseDraw) {
+    case 'lose':
+    console.log("Player 2 wins!\n");
+    playerTwoScore++;
+    break;
+    case 'draw':
+    console.log("It's a draw.\n");
+    break;
+    default:
+    console.log("Player 1 wins!\n");
+    playerOneScore++;
+  }
+}
+const whoWins = (playerOne, playerTwo) => {
+  switch (true) {
+    case playerOne === playerTwo:
+      result('draw');
+      break;
+      case playerOne === weapons[0] && playerTwo === weapons[2]:
+      result('win');
+      break;
+      case playerOne === weapons[1] && playerTwo === weapons[0]:
+      result('win');
+      break;
+      case playerOne === weapons[2] && playerTwo === weapons[1]:
+      result('win');
+      break;
+      default:
+      result('lose');
+  }
+}
+const playerOneMoves = [];
+const playerTwoMoves = [];
+let playerOneScore = 0;
+let playerTwoScore = 0;
+
+for (let i = 0; i < numberOfRounds; i++) {
+  playerOneMoves.push(weaponOfChoice());
+  playerTwoMoves.push(weaponOfChoice());
+  console.log(`Round ${i + 1}:`);
+  console.log(`Player 1: ${playerOneMoves[i]}`);
+  console.log(`Player 2: ${playerTwoMoves[i]}`);
+  whoWins(playerOneMoves[i], playerTwoMoves[i])
+}
+console.log(`Player 1 score: ${playerOneScore}`);
+console.log(`Player 2 score: ${playerTwoScore}`);
+console.log(playerOneScore > playerTwoScore ? "Player One wins the game!" : playerTwoScore > playerOneScore ? "Player Two wins the game!" : "This game is a draw.");
+
